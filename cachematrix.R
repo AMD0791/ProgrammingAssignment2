78f9143a7d414d5a91912d916f2bc09d098a7dc5
AMD0791 <andrea4618@gmail.com>
2015-05-23 22:11:35 -0700

    added code needed to make functions work.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a cached list of functions for a given matrix and
+## return the inverse of the matrix from that cache 
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix and creates a list to hold the functions
+## needed to store and return the inverse of that matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(cm = matrix()) {
 
+  inv <- NULL
+  mset <- function(newmatrix){
+    cm <<- newmatrix
+    inv <<- NULL
+  }
+  mget <- function() cm
+  setinv <- function(newinv) inv <<- newinv
+  getinv <- function() inv
+  list(mset=mset, mget=mget, setinv=setinv,getinv= getinv)
+  
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve 1. accepts a list created by makeCacheMatrix,
+##            2a. returns the value of the inverse if it exists or
+##            2b. calculates the inverse and stores it in the list, and
+##            3.  returns the inverse
 
-cacheSolve <- function(x, ...) {
+
+cacheSolve <- function(mCM, ...) {
         ## Return a matrix that is the inverse of 'x'
+  cs <- mCM$getinv()
+  if(!is.null(cs)) {
+    message("getting cached data")
+    return(cs)
+  }
+  newmat <- mCM$mget()
+  cs <- solve(newmat, ...)
+  mCM$setinv(cs)
+  cs
 }
